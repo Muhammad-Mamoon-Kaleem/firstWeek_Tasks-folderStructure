d18859a09a6b9486ea0c4cd2664371e9249a5a10
Muhammad-Mamoon-Kaleem <mamoonkaleem199@gmail.com>
2025-04-29 12:45:17 +0500

    Remove multer from superAdminRoutes.js

--- Routes/superAdminRoutes.js
+++ Routes/superAdminRoutes.js
@@ -1,15 +1,14 @@
 import express from 'express'
-import multer from 'multer';
 import Userauthentication from '../MiddleWares/userAuthenticationMiddleware.js';
 import { addNewAdminorUser, changeUserRole, changeUserstatus, getAlltaskbyAdmin, getAllusers } from '../Controllers/superAdminController.js';
 import { verifyConfirmationForNewAccount } from '../MiddleWares/confirmationMiddleware.js';
 import { historyMiddleware } from '../MiddleWares/historyTrackMiddleware.js';
 const superAdminRouter = express.Router();
-const midlleware = multer();
-superAdminRouter.get('/getallusers',midlleware.none(),Userauthentication('Please login before getting users data'),historyMiddleware,getAllusers)
-superAdminRouter.patch('/changeuserstatus',midlleware.none(),Userauthentication('Please login before changing user account status'),historyMiddleware,changeUserstatus)
-superAdminRouter.patch('/changeuserrole',midlleware.none(),Userauthentication('Please login before changing user role'),historyMiddleware,changeUserRole)
-superAdminRouter.get('/gettaskbyadmin',midlleware.none(),Userauthentication('plz login before getting data'),historyMiddleware,getAlltaskbyAdmin)
-superAdminRouter.post('/createnewaddminoruser',midlleware.none(),Userauthentication('plz login before'),historyMiddleware,addNewAdminorUser)
-superAdminRouter.post('/verifyandcretenewuseroradmin',midlleware.none(),historyMiddleware,verifyConfirmationForNewAccount)
+
+superAdminRouter.get('/getallusers',Userauthentication('Please login before getting users data'),historyMiddleware,getAllusers)
+superAdminRouter.patch('/changeuserstatus',Userauthentication('Please login before changing user account status'),historyMiddleware,changeUserstatus)
+superAdminRouter.patch('/changeuserrole',Userauthentication('Please login before changing user role'),historyMiddleware,changeUserRole)
+superAdminRouter.get('/gettaskbyadmin',Userauthentication('plz login before getting data'),historyMiddleware,getAlltaskbyAdmin)
+superAdminRouter.post('/createnewaddminoruser',Userauthentication('plz login before'),historyMiddleware,addNewAdminorUser)
+superAdminRouter.post('/verifyandcretenewuseroradmin',historyMiddleware,verifyConfirmationForNewAccount)
 export default superAdminRouter;
